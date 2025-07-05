c09914a11500c65267f889102fe363735abe41ef
averyhochheiser <163128643+averyhochheiser@users.noreply.github.com>
2025-07-05 16:11:45 +0000

    fixed path

--- js-server/index.js
+++ js-server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8001;
 
+app.get('/', (req, res) => {
+  res.send('Hello from Node!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
